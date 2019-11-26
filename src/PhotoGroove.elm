72d6d44d483866f4f38a30f5d15f5462222585f8
Yoshi Carroll <yoshi.carroll@dispatchhealth.com>
2019-11-25 19:31:30 -0700

    Add styles, and more stuff

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,11 +1,35 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Html exposing (div, h1, img, node, text)
 import Html.Attributes exposing (..)
 
 
+styles =
+    """
+body { background-color: rgb(44, 44, 44); color: white; }
+img { border: 1px solid white; margin: 5px; }
+.large { width: 500px; float: right; }
+.selected { margin: 0; border: 6px solid #60b5cc; }
+.content { margin: 40px auto; width: 960px; }
+#thumbnails { width: 440px; float: left; }
+h1 { font-family: Verdana; color: #60b5cc; }
+"""
+
+
+stylesNode =
+    node "style" [] [ text styles ]
+
+
 view model =
-    div [ class "content" ] [ h1 [] [ text "Photo Groove!" ] ]
+    div [ class "content" ]
+        [ stylesNode
+        , h1 [] [ text "Photo Groove!" ]
+        , div [ id "thumbnials" ]
+            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
+            , img [ src "http://elm-in-action.com/2.jpeg" ] []
+            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            ]
+        ]
 
 
 main =
